d96447de037fb82704548e23fa1b87ac4a13f931
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2017-04-13 19:23:56 +0200

    Basic MainWindow

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        SelectionOverlay so = new SelectionOverlay();
-        so.display();
+//        SelectionOverlay so = new SelectionOverlay();
+//        so.display();
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
     }
 }
