c7b79538fafde5c1fe4859fe3534686e6ae40156
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2017-04-15 18:19:36 +0200

    Generate output file name

--- src/main/java/recorder/ByzanzRecorder.java
+++ src/main/java/recorder/ByzanzRecorder.java
@@ -2,8 +2,12 @@ package recorder;
 
 import util.CommandBuilder;
 import util.DisplayManager;
+import util.SettingsManager;
 
 import java.awt.*;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ByzanzRecorder extends ScreenRecorder {
     private static final String DELAY_COMMAND = "sleep 191337";
@@ -16,20 +20,29 @@ public class ByzanzRecorder extends ScreenRecorder {
             GraphicsDevice gd = DisplayManager.getSourceDevice();
             Rectangle desktopBounds = gd.getDefaultConfiguration().getBounds();
 
-            int x = Math.min((int)sourceDesktopRectangle.getMaxX(), sourceDesktopRectangle.x) + desktopBounds.x;
-            int y = Math.min((int)sourceDesktopRectangle.getMaxY(), sourceDesktopRectangle.y) + desktopBounds.y;
+            int x = Math.min((int) sourceDesktopRectangle.getMaxX(), sourceDesktopRectangle.x) + desktopBounds.x;
+            int y = Math.min((int) sourceDesktopRectangle.getMaxY(), sourceDesktopRectangle.y) + desktopBounds.y;
             int width = Math.abs(sourceDesktopRectangle.width);
             int height = Math.abs(sourceDesktopRectangle.height);
 
             CommandBuilder commandBuilder = new CommandBuilder();
-            commandBuilder.setCommand("byzanz-record")
-                          .addParam("-x", x)
-                          .addParam("-y", y)
-                          .addParam("-w", width)
-                          .addParam("-h", height)
-                          .addParam("-e", DELAY_COMMAND)
-                          .addParam("-c")
-                          .addParam("/home/zaak/tmp/o.gif");
+            commandBuilder
+                    .setCommand("byzanz-record")
+                    .addParam("-x", x)
+                    .addParam("-y", y)
+                    .addParam("-w", width)
+                    .addParam("-h", height)
+                    .addParam("-e", DELAY_COMMAND);
+
+            if (SettingsManager.getRecordCursor()) {
+                commandBuilder.addParam("-c");
+            }
+
+            String targetFileName = "screenscast_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".gif";
+            String targetFilePath = Paths.get(SettingsManager.getOutputDirectoryPath(), targetFileName).toString();
+            commandBuilder.addParam(targetFilePath);
+
+            System.out.println("Running command: \n" + String.join(" ", commandBuilder.build()));
 
             ProcessBuilder pb = new ProcessBuilder(commandBuilder.build());
             pb.redirectOutput();
