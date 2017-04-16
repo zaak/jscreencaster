8e50a7f46c8e6f13d7575ac94410a97125d7296a
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2017-04-16 11:56:40 +0200

    Detect if byzanz is installed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,14 @@ import util.SettingsManager;
 
 import javax.swing.*;
 import javax.swing.UIManager.*;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
+        if(!isByzanzInstalled()) {
+            return;
+        }
+
         try {
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -21,5 +26,17 @@ public class App {
 
         MainWindow mw = new MainWindow();
         mw.setVisible(true);
+    }
+
+    private static boolean isByzanzInstalled() {
+        try {
+            Runtime.getRuntime().exec("byzanz-record");
+            return true;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Byzanz executable not found.\nThe application will now close.");
+            System.err.println(e.toString());
+        }
+
+        return false;
     }
 }
